3fa80cab23f7ce51b188b57b2cb535209f0fa88b
Krishnaprasath Venkadesan <salzarprince@gmail.com>
2025-01-01 20:05:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Declare the numbers using var
+var num1 = 5;
+var num2 = 10;
+var num3 = 15;
+var num4 = 20;
+var num5 = 25;
+
+// Add the numbers
+var sum = num1 + num2 + num3 + num4 + num5;
+console.log("The sum of the numbers is: " + sum);
+
+// Subtract 40 from the sum
+var sub = sum - 40;
+console.log("The difference of the numbers is: " + sub);
+
+// Multiply the result by 30
+var result = sub * 30;
+console.log("The multiplication of the result is: " + result);
